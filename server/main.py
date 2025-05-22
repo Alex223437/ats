6a7cd952e43601ac0d9c6afe7f0ed35f2365eaa8
Alex223437 <alex223437@gmail.com>
2025-05-22 15:40:39 +0200

    cors-fix?

--- server/main.py
+++ server/main.py
@@ -27,7 +27,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ats-client-sidoryk.onrender.com/", "http://localhost:5173"],
+    allow_origins=["https://ats-client-sidoryk.onrender.com", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
